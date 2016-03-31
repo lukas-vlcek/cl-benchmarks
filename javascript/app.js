5a82a69ec91af30ab664901aab9c99c68e37ca09
Lukáš Vlček <lukas.vlcek@gmail.com>
2016-03-31 14:09:13 +0200

    Add first dummy chart.

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lvlcek on 31/03/16.
+ */
+(function (context) {
+
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
+        }
+    });
+
+})(this);
