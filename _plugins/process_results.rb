982fd25fd07614c2433b2e0cdd5852444fee17ed
Lukáš Vlček <lukas.vlcek@gmail.com>
2016-04-01 12:12:48 +0200

    Renamed tests and added metadata.

--- _plugins/process_results.rb
+++ _plugins/process_results.rb
@@ -7,7 +7,7 @@ module Jekyll
     # priority :low
 
     def generate(site)
-      Jekyll.logger.info "\n--------------"
+      # Jekyll.logger.info "\n--------------"
       # site.collections.each do |name, collection|
       # end
 
@@ -20,11 +20,26 @@ module Jekyll
         path = path.parent
       end
 
+      # this is the root where we store tests
+      tests = site.data["tests"]
+
       # get unique list of performance tests
       path.children.each do |d|
-        Jekyll.logger.info "- #{d.basename}"
+        test_key = String(d.basename)
+        # Jekyll.logger.info "- #{test_key}"
+        tests[test_key] = {}
+        tests[test_key]["code"] = test_key # we must make sure key is string, or Liquid will screw up...
       end
 
+      Jekyll.logger.info "--------"
+      Jekyll.logger.info "tests: #{tests}"
+      # for k in site.data["tests"].keys
+      #   Jekyll.logger.info "- #{tests[k]}"
+      # end
+
+      tests_metadata = site.data["tests_metadata"]
+      Jekyll.logger.info "tests_metadata: #{tests_metadata}"
+      Jekyll.logger.info "--------"
     end
 
   end
