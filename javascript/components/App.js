bd68e183ca0bd6f1a1c7e85c2facffcd82c96720
Lukáš Vlček <lukas.vlcek@gmail.com>
2016-04-14 14:39:49 +0200

    Update

--- javascript/components/App.js
+++ javascript/components/App.js
@@ -1,14 +1,17 @@
 var AppComponent = React.createClass({
     displayName: "AppComponent",
     render: function() {
-        console.log("this.state:", this.state);
+        // console.log("this.state:", this.state);
         // console.log('this.props:', this.props);
 
-        var reactElement = (this.state && this.state.params && this.state.params.view === AppConstant.VIEW_DETAIL) ?
-            React.createElement(DetailComponent, {}) :
-            React.createElement(MasterComponent, {
+        var reactElement = null;
+        if (this.state && this.state.path && this.state.path.substr(1).startsWith(AppConstant.VIEW_DETAIL)) {
+            reactElement = React.createElement(DetailComponent, this.state.params)
+        } else {
+            reactElement = React.createElement(MasterComponent, {
                 chartClickHandler: this.props.chartClickHandler
             });
+        }
 
         return reactElement;
     }
